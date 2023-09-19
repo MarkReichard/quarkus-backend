2e4bb13daeebb21d49065fa17fd5a83f9ef638f1
Mark Reichard <mark@idatadc.com>
2023-09-19 14:22:17 -0400

    TESTING WEBHOOK

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -30,7 +30,7 @@ public class Post extends PanacheMongoEntity {
 
     public String getContent() {
         //return new StringBuilder(content).reverse().toString(); // Should be: return content;
-        return content + "TEST";
+        return content + "TEST2";
     }
     public Long getTimestamp() {
         return timestamp;
